00c30aa4d8899da71803a12401f2123fb2e4845e
Komod Wang <komod.Wang@gmail.com>
2017-03-15 15:32:25 +0800

    Implement multiple questions function.

--- GeoQuiz/app/src/main/java/com/example/komod/geoquiz/QuizActivity.java
+++ GeoQuiz/app/src/main/java/com/example/komod/geoquiz/QuizActivity.java
@@ -3,29 +3,59 @@ package com.example.komod.geoquiz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
-    private Button mTrueButton;
-    private Button mFalseButton;
+    private TextView mQuestionTextView;
+
+    private TrueFalse[] mQuestionBank = new TrueFalse[] {
+            new TrueFalse(R.string.question_tainan, true),
+            new TrueFalse(R.string.question_japan, false),
+            new TrueFalse(R.string.question_korea, true),
+            new TrueFalse(R.string.question_china, false),
+    };
+
+    private int mCurrentIndex = 0;
+
+    private void updateQuestionText() {
+        mQuestionTextView.setText(mQuestionBank[mCurrentIndex].getQuestion());
+    }
+
+    private void checkAnswer(boolean userAnswer) {
+        if (userAnswer == mQuestionBank[mCurrentIndex].isTrueQuestion()) {
+            Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
-        mTrueButton = (Button)findViewById(R.id.true_button);
-        mFalseButton = (Button)findViewById(R.id.false_button);
+        mQuestionTextView = (TextView)findViewById(R.id.question_text_view);
+        updateQuestionText();
 
-        mTrueButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.true_button).setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(true);
             }
         });
-        mFalseButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.false_button).setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View v) {
-                Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+                checkAnswer(false);
+            }
+        });
+        findViewById(R.id.next_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestionText();
             }
         });
     }
